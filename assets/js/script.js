e882435a1896f2ce749e35bb3072975f4ffb8711
shaggy <simmons5family@gmail.com>
2020-10-19 21:17:19 -0500

    magically no console errors now????

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,24 +56,24 @@ $(".list-group").on("click", "p", function() {
   textInput.trigger("focus");
 })
 $(".list-group").on("blur", "textarea", function() {
-  // get the textarea's current value/text
-  var text = $(this)
-  .val()
-  .trim();
+ // get the textarea's current value/text
+var text = $(this)
+.val()
+.trim();
 
-  // get the parent ul's id attribute
-  var status = $(this)
-  .closest(".list-group-item")
-  .attr("id")
-  .replace("list-", "");
+// get the parent ul's id attribute
+var status = $(this)
+.closest(".list-group")
+.attr("id")
+.replace("list-", "");
 
-  // get the task's position in the list of other li elements
-  var index = $(this)
-  .closest(".list-group-item")
-  .index();
+// get the task's position in the list of other li elements
+var index = $(this)
+.closest(".list-group-item")
+.index();
 
-  tasks[status][index].text = text;
-  saveTasks();
+tasks[status][index].text = text;
+saveTasks();
 
   // recreate p element
   var taskP = $("<p>")
