b39b69cbe725bfe2fdf2f2a25645a8a11caef8b5
shaggy <simmons5family@gmail.com>
2020-10-22 17:36:09 -0500

    checks date every 30min for color change

--- assets/js/script.js
+++ assets/js/script.js
@@ -172,6 +172,12 @@ $("#modalDueDate").datepicker({
   minDate: 1
 });
 
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  })
+}, (1000 * 60) * 30);
+
 
 $(".card .list-group").sortable({
   connectWith: $(".card .list-group"),
